02b938a6c0c1cd9d79ac89de7c566da83da9b5ab
Miguel Angel Bautista <Miguel Angel Bautista>
2017-05-03 17:54:15 -0500

    Iniciando rutas

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,40 @@
 'use strict';
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const Dependencia = require('../models/dependencia'),
+    Comentario = require('../models/comentario'),
+    Sucursal = require('../models/sucursal'),
+    Tramite = require('../models/tramite'),
+    Categoria = require('../models/categoria');
 
-router.get("/",(req,res,next)=>{
-    //res.json({'data':[{'algo':'direcicion'}]})
-});
+
+router.get("/categorias", (req, res, next) => {
+    Categoria.findAll({sort:'id'})
+    .then((d)=>{
+        if(d){
+            res.status(200)
+            .json({categorias:d})
+        }else{
+            res.status(200)
+            .json({message:"No hay datos a mostrar"});
+        }
+    });
+}).
+get('/busqueda/:filtro',(req,res,next)=>{
+    const filtro=req.params.filtro;
+    Tramite.findAll({
+        where:{
+            $or: [{'nombre': {$iLike: `%${filtro}`}}, 
+                    {'descripcion':{$iLike: `%${filtro}`}}]
+        }
+    }).then((data)=>{
+        console.log(data);
+    });
+})
+//{idtramit,nombre,dependencia,categoria}
+
+;
 
 
-module.exports=router;
+
+module.exports = router;
