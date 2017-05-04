d933efcfcd10b9b66f724f16b84b59925925cf55
Juan Pablo <esponja.014@gmail.com>
2017-05-03 19:31:36 -0500

    Añadido mapas

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -22,3 +22,48 @@ app.config(['$routeProvider',
         redirectTo: '/'
       });
   }]);
+
+function initMap(data) {
+  if(data){
+    var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4
+            });
+    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    data.forEach( function(element, index) {
+      const array = element.coordenadas.split(",");
+      var posicion = new google.maps.LatLng(array[0],array[1]);
+      var marr = new google.maps.Marker({
+          map: map
+      });
+    });
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var lol = new google.maps.Marker({
+          position: pos,
+          map: map
+        });
+        map.center(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+    }else{
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: El servicio de Geolocalización falló.' :
+                              'Error: Tu navegador no soporta geolocalización.');
+    }
+  }else{
+    document.getElementById("mapa").innerHtml("<h1>Error</h1><h3>No se ha encontrado el recurso</h3>")
+  }
+}
